925009f2731ec3a3845514e48a3ae4718558f4b7
JakubKisiel <kisieljaku@gmail.com>
2021-04-14 18:53:20 +0200

    added route with 4 methods to main.py /methods uri

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.post("/method")
+def method():
+    return{"method": "POST"}
+
+@app.get("/method")
+def method():
+    return{"method": "GET"}
+
+@app.put("/method")
+def method():
+    return{"method": "PUT"}
+
+@app.delete("/method")
+def method():
+    return{"method": "DELETE"}
