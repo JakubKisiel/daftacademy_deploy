1149331e68c7c1b83bcb9184036b9c197b66cee8
JakubKisiel <kisieljaku@gmail.com>
2021-05-11 17:06:26 +0200

    added new endpoint for /employees that takes 3 params limit offset and order

--- views/zad4.py
+++ views/zad4.py
@@ -32,7 +32,7 @@ async def categories():
 
 @zad4.get("/products/{id}")
 async def products_id(id: int):
-
+    zad4.db_connection.row_factory = aiosqlite.Row
     data = await zad4.db_connection.execute("""
     SELECT ProductID id, ProductName name
     FROM Products
@@ -42,4 +42,24 @@ async def products_id(id: int):
     if not data:
         raise HTTPException(status_code=404, detail="Id not found")
     return data
+
+EMP_SET = {"order", "first_name", "last_name", "city"}
+
+@zad4.get("/employees")
+async def employees(limit: int = 0, offset: int= 0, order: str = None):
+    if order and order not in EMP_SET:
+        raise HTTPException(status_code=400)
+    order = order if order else "id"
+    zad4.db_connection.row_factory = aiosqlite.Row
+    data = await zad4.db_connection.execute("""
+    SELECT EmployeeID id, LastName last_name, FirstName first_name, City city
+    FROM Employees
+    ORDER BY ?
+    LIMIT ?
+    OFFSET ?
+    """, (order, limit, offset))
+    return {"employees": data}
+    
+
+
 
