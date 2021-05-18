2e6fe99f064ddaa29612cac4faf8288fa0580b17
JakubKisiel <kisieljaku@gmail.com>
2021-05-18 20:21:12 +0200

    Added endpoint for 5.3

--- app/crud.py
+++ app/crud.py
@@ -11,3 +11,17 @@ def get_supplier(db: Session, supplier_id: int):
 def get_supplier_products(db: Session, supplier_id:int):
     return db.query(models.Product).join(models.Category).filter(models.Product.SupplierID == supplier_id).order_by(models.Product.ProductID.desc()).all()
 
+def create_new_supplier(db: Session, new_supplier: NewSupplier):
+    new_supplier_id = db.query(models.Supplier).count()+1
+    new_supplier_output = models.Supplier(SupplierID = new_supplier_id,
+                                          CompanyName = new_supplier.CompanyName,
+                                          ContactName = new_supplier.ContactName,
+                                          ContactTitle = new_supplier.ContactTitle,
+                                          Address = new_supplier.Address,
+                                          City = new_supplier.City,
+                                          PostalCode = new_supplier.PostalCode,
+                                          Country = new_supplier.Country,
+                                          Phone = new_supplier.Phone)
+    db.add(new_supplier_output)
+    db.commit()
+    return new_supplier_output
--- app/schemas.py
+++ app/schemas.py
@@ -40,3 +40,16 @@ class SupplierProduct(BaseModel):
     
     class Config:
         orm_mode = True
+
+class NewSupplier(BaseModel):
+    CompanyName: str
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
+    Address: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
+    
+    class Config:
+        orm_mode = True
