d52ba2066cbfccab9c97c0301738165b28be783a
JakubKisiel <kisieljaku@gmail.com>
2021-05-19 09:25:24 +0200

    fixed bug for 5.2

--- app/views/zad5.py
+++ app/views/zad5.py
@@ -16,14 +16,14 @@ async def get_all_suppliers(db: Session = Depends(get_db)):
 async def get_supplier(supplier_id: PositiveInt, db: Session = Depends(get_db)):
     db_supplier = crud.get_supplier(db, supplier_id)
     if db_supplier is None:
-        raise HTTPException(status_code=404, detail="Supplier not found")
+        raise HTTPException(status_code=404, detail="not found")
     return db_supplier
 
 @router.get("/suppliers/{supplier_id}/products", response_model=List[schemas.SupplierProduct])
 async def get_supplier_products(supplier_id: PositiveInt, db: Session = Depends(get_db)):
     db_products = crud.get_supplier_products(db, supplier_id)
-    if db_products is None:
-        raise HTTPException(status_code=404, detail="Supplier not found")
+    if len(db_products)==0 :
+        raise HTTPException(status_code=404, detail="not found")
     return db_products
 
 @router.post("/suppliers", response_model=schemas.Supplier, status_code=201)
